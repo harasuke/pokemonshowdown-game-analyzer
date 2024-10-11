f8114db7ec5128c6c814a2489b6d0f7f4482aa1c
Spinato Mirko <mirko.spinato@iclod.com>
2024-10-11 10:40:56 +0200

    Damage calcs NO passive status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, computed, signal, Signal, WritableSignal } from '@angular/core';
-import { FormArray, FormControl, FormGroup, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  UntypedFormBuilder,
+  UntypedFormGroup
+} from '@angular/forms';
 import { filter, forkJoin, map, take } from 'rxjs';
 import { cloneDeep } from 'lodash';
 import { GameStats, PlayerData, PlayerTeam, PokemonData } from './core/interfaces';
@@ -16,7 +22,6 @@ export function escapeRegExp(text: string) {
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
   formItem: UntypedFormGroup;
 
   constructor(private formBuilder: UntypedFormBuilder, private http: HttpClient) {
@@ -27,10 +32,10 @@ export class AppComponent {
     this.formItem.get('gameurls')?.valueChanges.subscribe((gameUrls) => {
       let urls = gameUrls.split('\n');
       urls = urls.map((url: string) => {
-        return url.split('/').pop();
+        return url.split('/').pop()?.split('battle-').pop()
       });
-      this.loadData(urls)
-    })
+      this.loadData(urls);
+    });
   }
 
   title = 'pokemonshowdown-game-analyzer';
@@ -38,37 +43,34 @@ export class AppComponent {
   selectedPokemon: WritableSignal<string> = signal('');
   loading: WritableSignal<boolean> = signal(false);
 
-  players: WritableSignal<{[player: string]: PlayerData}|null> = signal(null);
+  players: WritableSignal<{ [player: string]: PlayerData } | null> = signal(null);
   pokemonList: WritableSignal<string[]> = signal([]);
-  playerPokemons:Signal<string[]|null> = computed(() => {
+  playerPokemons: Signal<string[] | null> = computed(() => {
     if (this.selectedPlayer() === '') return null;
 
-    return Object.keys(this.players()![this.selectedPlayer()]['team']).filter((key) => 
-      this.players()![this.selectedPlayer()]['team'][key].aliases[0] == key
+    return Object.keys(this.players()![this.selectedPlayer()]['team']).filter(
+      (key) => this.players()![this.selectedPlayer()]['team'][key].aliases[0] == key
     );
   });
 
   loadData(urls: string[]) {
     this.loading.set(true);
     const domParser = new DOMParser();
-    
-    // const apiCalls = urls.map(url => 
+
+    // const apiCalls = urls.map(url =>
     //   this.http.get<any>(`/replay/${url}.log`, { responseType: "text" as "json" })
     // );
 
     const apiCalls = urls.map((url) =>
-      this.http.get<any>(
-        `/replay/${url}.log`, 
-        { responseType: "text" as "json" }
-      ).pipe(
-        map((response) => ({ url, response }))
-      )
+      this.http
+        .get<any>(`/replay/${url}.log`, { responseType: 'text' as 'json' })
+        .pipe(map((response) => ({ url, response })))
     );
 
     forkJoin(apiCalls)
       .pipe(take(1))
       .subscribe((games) => {
-        games.forEach(data => {
+        games.forEach((data) => {
           this.initializeObjects(data.response, data.url);
         });
         this.loading.set(false);
@@ -77,24 +79,48 @@ export class AppComponent {
 
   initializeObjects(data: string, gameUrl: string) {
     const battleLog: string[] = data.split('\n') ?? [];
-    const p1name = battleLog.find(row => row.includes('|player|p1'))?.split('|')[3] ?? undefined;
-    const p2name = battleLog.find(row => row.includes('|player|p2'))?.split('|')[3] ?? undefined;
+    const p1name = battleLog.find((row) => row.includes('|player|p1'))?.split('|')[3] ?? undefined;
+    const p2name = battleLog.find((row) => row.includes('|player|p2'))?.split('|')[3] ?? undefined;
     if (p1name === undefined || p2name === undefined) return;
 
-    let p1pkmn: PlayerTeam = battleLog.filter(row => row.includes('|poke|p1')).map(row => row.split('|')[3]).reduce((acc: any, curr: string) => {acc[curr] = { aliases: [curr], turnsOnField: 0, kills: 0, supportMoves: 0, overallUtility: {}}; return acc}, {});
-    let p2pkmn: PlayerTeam = battleLog.filter(row => row.includes('|poke|p2')).map(row => row.split('|')[3]).reduce((acc: any, curr: string) => {acc[curr] = { aliases: [curr], turnsOnField: 0, kills: 0, supportMoves: 0, overallUtility: {}}; return acc}, {});
+    let p1pkmn: PlayerTeam = battleLog
+      .filter((row) => row.includes('|poke|p1'))
+      .map((row) => row.split('|')[3])
+      .reduce((acc: any, curr: string) => {
+        acc[curr] = {
+          aliases: [curr],
+          turnsOnField: 0,
+          kills: 0,
+          supportMoves: 0,
+          overallUtility: {}
+        };
+        return acc;
+      }, {});
+    let p2pkmn: PlayerTeam = battleLog
+      .filter((row) => row.includes('|poke|p2'))
+      .map((row) => row.split('|')[3])
+      .reduce((acc: any, curr: string) => {
+        acc[curr] = {
+          aliases: [curr],
+          turnsOnField: 0,
+          kills: 0,
+          supportMoves: 0,
+          overallUtility: {}
+        };
+        return acc;
+      }, {});
     p1pkmn = this.elaboratePokemonData(p1pkmn, battleLog, gameUrl);
     p2pkmn = this.elaboratePokemonData(p2pkmn, battleLog, gameUrl);
 
-    this.players.update(g => {
+    this.players.update((g) => {
       if (g === null) g = {};
       if (!!!g[p1name]) {
-        g[p1name] = { team: {} }
-        g[p1name]['team'] = p1pkmn
+        g[p1name] = { team: {} };
+        g[p1name]['team'] = p1pkmn;
       }
       if (!!!g[p2name]) {
-        g[p2name] = { team: {} }
-        g[p2name]['team'] = p2pkmn
+        g[p2name] = { team: {} };
+        g[p2name]['team'] = p2pkmn;
       }
       return g;
     });
@@ -103,11 +129,10 @@ export class AppComponent {
 
   elaboratePokemonData(pokemonTeam: PlayerTeam, battleLog: string[], gameUrl: string) {
     pokemonTeam = Object.fromEntries(
-      Object.entries(pokemonTeam)
-        .map(([pkmnName, pkmnData]) => {
-          return this.analyzeData(pkmnName, pkmnData, battleLog, gameUrl)
-        })
-      );
+      Object.entries(pokemonTeam).map(([pkmnName, pkmnData]) => {
+        return this.analyzeData(pkmnName, pkmnData, battleLog, gameUrl);
+      })
+    );
     Object.values(pokemonTeam).forEach((pkmnData: any) => {
       const surname = pkmnData['aliases'][1] as string;
       pokemonTeam[surname] = pkmnData;
@@ -116,62 +141,171 @@ export class AppComponent {
   }
 
   analyzeData(pkmnName: string, pkmnData: PokemonData, battlelog: string[], gameUrl: string) {
-    const pkmnSurname = battlelog.find((row) => row.includes('|switch|') && row.includes(pkmnName))?.split('|')[2].split(': ')[1]
+    const pkmnSurname = battlelog
+      .find((row) => row.includes('|switch|') && row.includes(pkmnName))
+      ?.split('|')[2]
+      .split(': ')[1];
     if (!!pkmnSurname) pkmnData['aliases'].push(pkmnSurname);
-    console.log('analyzing the pokemon: ', pkmnData.aliases[1] ?? pkmnData.aliases[0])
+    console.log('START ANALYSIS OF: ', pkmnData.aliases[1] ?? pkmnData.aliases[0]);
     const gameUrlId: string = gameUrl.split('/').pop()!.split('.log')[0];
     pkmnData.turnsOnField = this.getTurnOnField(pkmnName, pkmnData, battlelog);
-    pkmnData.overallUtility[gameUrlId] = this.getOverallUtility(pkmnData.aliases[1] ?? pkmnData.aliases[0], pkmnData, battlelog);
-    
+    pkmnData.overallUtility[gameUrlId] = this.getOverallUtility(
+      pkmnData.aliases[1] ?? pkmnData.aliases[0],
+      pkmnData,
+      battlelog
+    );
+
     return [pkmnName, pkmnData];
   }
 
   getTurnOnField(pkmnName: string, pkmnData: PokemonData, battleLog: string[]) {
-    return battleLog.slice(
-      battleLog.findIndex(row => row.includes('|turn|1'))
-    ).filter(row => row.includes('|turn|') || row.includes(pkmnName)).length;
+    return battleLog
+      .slice(battleLog.findIndex((row) => row.includes('|turn|1')))
+      .filter((row) => row.includes('|turn|') || row.includes(pkmnName)).length;
   }
 
   getOverallUtility(pkmnName: string, pkmnData: PokemonData, battleLog: string[]): GameStats {
     // const moveRegex = new RegExp(`^\\|move\\|[^|]+\\|[^:]+: ${escapeRegExp(pkmnName)}`);
     const moveRegex = new RegExp(`^\\|move\\|[^:]+: ${escapeRegExp(pkmnName)}`);
-    const movesDone = battleLog.filter(row => moveRegex.test(row));
+    const movesDone = battleLog
+      .map((row, idx) => ({ row, idx }))
+      .filter((row) => moveRegex.test(row.row));
     let totalDamage = 0;
-    movesDone.forEach(move => {
-      console.log('analyzing row>', move);
-      const moveInTurn = battleLog.findIndex(row => row === move);
-      const whoDamaged = battleLog[moveInTurn].split('|')[4];
-      const pkmnNameWithPlayerN = battleLog[moveInTurn].split('|')[2];
-      console.log('and it damaged', whoDamaged);
-      if (whoDamaged === pkmnNameWithPlayerN) return console.log('it was a non damaging move');
-      // let whoDamagedPrevHealth = battleLog.slice(0, moveInTurn).reverse().find(row => row.includes(whoDamaged) && (row.includes('|-damage|') || row.includes('|-heal|')))?.split('|')[3]?.split('/')[0]
-      // if (whoDamagedPrevHealth === undefined) whoDamagedPrevHealth = '100';
-      let whoDamagedPrevHealth = this.searchForPreviousHealth(whoDamaged, battleLog.slice(0, moveInTurn));
-      console.log('the pokemon ',pkmnName, 'damaged ', whoDamaged, 'that had ', whoDamagedPrevHealth, 'hp befor taking damage')
-       
-      const attackFinished = battleLog.slice(moveInTurn).findIndex(row => row.includes('|move|'))
-      const whereToSearchForDamage = battleLog.slice(moveInTurn, moveInTurn + attackFinished);
-      const damageOnTurn = whereToSearchForDamage.filter(row => 
-        row.includes('|-damage|') && row.split('|')[2] == whoDamaged)
-          .map(row => {
-            return row.split('|').pop()
-          })
-          .map(damage => {
-            if (damage?.includes('fnt'))
-              return Number(whoDamagedPrevHealth)
-            else
-              return Number(whoDamagedPrevHealth) - Number(damage?.split('/')[0])
-          })
-          .reduce((acc, dmg) => acc + dmg, totalDamage);
-      console.log('the damage dealt is', damageOnTurn)
-    })
-    console.log(movesDone, totalDamage);
-    return {damageDone: totalDamage};
+    console.log('moves done', movesDone);
+    movesDone.forEach(({row: move, idx: moveIndex}) => {
+      // battleLog.filter((row, rowIndex) => row === move);
+      // const rowIndexWithMove = battleLog.findIndex((row) => row === move);
+      const rowIndexWithMove = moveIndex;
+      console.log('analyzing row>', move, ' with rowIndex', rowIndexWithMove);
+      const whoIsDamaged = battleLog[rowIndexWithMove].split('|')[4];
+      const pkmnNameWithPlayerN = battleLog[rowIndexWithMove].split('|')[2];
+      console.log('and it damaged', whoIsDamaged);
+      if (whoIsDamaged === pkmnNameWithPlayerN) return console.log('it was a non damaging move');
+      let whoIsDamagedPrevHealth = this.searchForPreviousHealth(
+        whoIsDamaged,
+        battleLog.slice(0, rowIndexWithMove)
+      );
+      console.log(
+        'the pokemon ',
+        pkmnName,
+        'damaged ',
+        whoIsDamaged,
+        'that had ',
+        whoIsDamagedPrevHealth,
+        'hp befor taking damage'
+      );
+
+      if (move.includes('[spread]')) {
+        const pkmnHitBySpread = move.split('|').pop()?.replace('[spread] ', '').split(',');
+        console.log('spread move', move);
+        pkmnHitBySpread?.forEach((pkmnSlot) => {
+          const _whoIsDamaged =
+            battleLog
+              .slice(rowIndexWithMove)
+              .find(
+                (row) => row.includes('|-heal|' + pkmnSlot) || row.includes('|-damage|' + pkmnSlot)
+              )
+              ?.split('|')[2] ?? pkmnSlot;
+          console.log('hit', _whoIsDamaged);
+          const prevHealth = this.searchForPreviousHealth(
+            _whoIsDamaged,
+            battleLog.slice(0, rowIndexWithMove)
+          );
+          console.log('damaged in spread', _whoIsDamaged, ' with previous health: ', prevHealth);
+          totalDamage = this.calculateDamage(
+            totalDamage,
+            _whoIsDamaged,
+            prevHealth,
+            battleLog,
+            rowIndexWithMove
+          );
+        });
+      } else {
+        totalDamage = this.calculateDamage(
+          totalDamage,
+          whoIsDamaged,
+          whoIsDamagedPrevHealth,
+          battleLog,
+          rowIndexWithMove
+        );
+      }
+
+      console.log('damage shoudl be', totalDamage);
+    });
+    return { damageDone: totalDamage };
   }
 
-  searchForPreviousHealth(whoIsDamaged: string, battleLog: string[]) {
-    let previousHealth = battleLog.reverse().find(row => row.includes(whoIsDamaged) && (row.includes('|-damage|') || row.includes('|-heal|')))?.split('|')[3]?.split('/')[0]
+  private calculateDamage(
+    totalDamage: number,
+    whoIsDamaged: string,
+    whoIsDamagedPrevHealth: number,
+    battleLog: string[],
+    rowIndexWithMove: number
+  ) {
+    /**
+     * starting in moveInTurn+1 to check when the next move starts.
+     * When a new |move| appears, it means the old one ended.
+     */
+    const rowIndexWhenAttackFinished = battleLog
+      .slice(rowIndexWithMove + 1)
+      .findIndex((row) => row.includes('|move|'));
+    const whereToSearchForDamage = battleLog.slice(
+      rowIndexWithMove,
+      rowIndexWithMove + rowIndexWhenAttackFinished + 1
+    );
+    console.log('searching in rows', whereToSearchForDamage);
+    const previousHealth = whereToSearchForDamage
+      .filter((row) => row.includes('|-damage|') && row.split('|')[2] == whoIsDamaged)
+      .map((row) => {
+        console.log('view health drop', row.split('|').pop());
+        return row.split('|').pop();
+      })
+      .reverse()
+      .map((healthRemaining) => {
+        /**
+         * Inverto l'ordine perche' se l'ultima riga include 'fnt', non ho bisogno di calcolare tutti i turni di hit.
+         * Il danno multi-attack ha ucciso il pokemon e quindi vuol dire che ha fatto tutta la vita rimasta.
+         */
+        if (healthRemaining?.includes('fnt')) {
+          return whoIsDamagedPrevHealth;
+        }
+        return Number(healthRemaining?.split('/')[0]);
+      });
+    // .map(damage => {
+
+    //   if (!damage?.includes('fnt')) {
+    //     console.log('prev health was', whoIsDamagedPrevHealth, 'resulting health is', damage?.split('/')[0])
+    //     return Number(damage?.split('/')[0]);
+    //     // whoIsDamagedPrevHealth -= Number(damage?.split('/')[0]);
+    //   }
+
+    //   console.log('dealt with this move', whoIsDamagedPrevHealth);
+    //   return whoIsDamagedPrevHealth;
+    // });
+    console.log('mmhh', previousHealth);
+    if (previousHealth.length > 1) {
+      totalDamage += previousHealth.pop()! - previousHealth[0]!;
+      // previousHealth.reduce((acc, dmg) => acc + dmg, totalDamage);
+    } else if (previousHealth.length === 1) {
+      if (whoIsDamagedPrevHealth === previousHealth[0]) totalDamage += whoIsDamagedPrevHealth;
+      else totalDamage += whoIsDamagedPrevHealth - previousHealth[0];
+    }
+    return totalDamage;
+  }
+
+  private searchForPreviousHealth(whoIsDamaged: string, battleLog: string[]): number {
+    let rowWithPrevHealth = battleLog
+      .reverse()
+      .find(
+        (row) =>
+          row.includes(whoIsDamaged) &&
+          (row.includes('|-damage|') || row.includes('|-heal|') || row.includes('|switch|'))
+      );
+    let previousHealth = rowWithPrevHealth?.includes('|switch|')
+      ? rowWithPrevHealth?.split('|')[4]?.split('/')[0]
+      : rowWithPrevHealth?.split('|')[3]?.split('/')[0];
     if (previousHealth === undefined) previousHealth = '100';
-    return previousHealth;
+    return Number(previousHealth);
   }
 }
+
